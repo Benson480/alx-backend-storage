d16012debe5da58531aa3d85b5c4378982b7b17b
Benson <bensonmwangi101@gmail.com>
2023-07-20 01:10:59 -0700

    Task 9 ammended

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" A Python function that inserts a new document in a collection based on kwargs """
+""" Insert document """
 import pymongo
 
 
@@ -7,12 +7,12 @@ def insert_school(mongo_collection, **kwargs):
     """ Insert a school with features
 
         Args:
-            Mongo_collection: Collection to pass
+            mongo_collection: Collection to pass
             kwargs: Dictionary with elements to put
 
         Return:
             Id of the new element
     """
-     new_school = mongo_collection.insert_one(kwargs)
+    new_school = mongo_collection.insert_one(kwargs)
 
-     return (new_school.insert_id)
+    return (new_school.inserted_id)
